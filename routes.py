60b9997c0e9228bdee6c7a425670b9b583ac4a8a
xPartyBear <michaeldekoski@gmail.com>
2022-06-01 16:38:49 -0400

    AWS user table, and password hashing

--- routes.py
+++ routes.py
@@ -1,41 +1,88 @@
-from flask import Flask, render_template, redirect, url_for, request
+from inspect import signature
+from flask import Flask, render_template, redirect, url_for, request, flash
+import boto3
+from botocore.config import Config
+import bcrypt
 
+client = boto3.client('dynamodb')
 app = Flask(__name__)
+
+my_config = Config(region_name='us-east-1',
+					signature_version='v4',
+					retries={'max_attempts': 10, 'mode': 'standard'})
+client = boto3.client('dynamodb', config=my_config)
 
 @app.route('/')
 def default():
 	return redirect(url_for('chatroom'))
 	
-@app.route('/chatroom')
+@app.route('/chatroom', methods=['GET', 'POST'])
 def chatroom():
-	# TODO check cookie user logged in
+	# if request.method == 'POST':
 	return render_template('chatroom.html', page_name='chatroom')
 	
 @app.route('/chatroom/login', methods=['GET', 'POST'])
 def login():
-	if request.method == "GET":
+	if request.method == 'GET':
 		return render_template('login.html', page_name='login')
 	else:
 		username = request.form['username']
 		password = request.form['password']
-		# TODO query for user in database
-		# if user not found: return error message 'User not found' (possibly error page that automatically redirects)
-		# if password doesn't match: error message 'Incorrect Password'
-		# if user found and password matches: render chatroom, logged in (create cookie)
-		return 'Username: %s\nPassword: %s' % (username, password)
-	
+		saltyTears = bcrypt.gensalt()
+		pwo= bcrypt.hashpw(request.form['password'].encode('utf-8'), saltyTears)
+		response = client.get_item(
+			TableName='users',
+			Key={
+				'username': {
+					'S': username
+				}
+			}
+		)
+		if 'Item' in response:
+			if bcrypt.checkpw(password.encode('utf-8'), response['Item']['password']['B']):
+				return redirect(url_for('chatroom'))
+			else:
+				flash('Incorrect password')
+		else:
+			flash('User not found')
+		return render_template('login.html', page_name='login', username=username, password=password)
+
 @app.route('/chatroom/signup', methods=['GET', 'POST'])
 def signup():
-	if request.method == "GET":
+	if request.method == 'GET':
 		return render_template('signup.html', page_name='signup')
 	else:
 		username = request.form['username']
-		password = request.form['password']
-		# TODO query for username in database
-		# if user exists: error message 'User already exists'
-		# if user not found: create new database user, success message with redirect to chatroom logged in
-		return 'Username: %s\nPassword: %s' % (username, password)
-
+		# password = request.form['password']
+		saltyTears = bcrypt.gensalt()
+		pwo = bcrypt.hashpw(request.form['password'].encode('utf-8'), saltyTears)
+		response = client.get_item(
+			TableName='users',
+			Key={
+				'username': {
+					'S': username
+				}
+			}
+		)
+		if 'Item' in response:
+			flash('User already exists')
+		else:
+			response = client.put_item(
+				TableName='users',
+				Item={
+					'username': {
+						'S': username
+					},
+					'password': {
+						'B': pwo
+					},
+					'tears': {
+						'B': saltyTears
+					}
+				}
+			)
+			return redirect(url_for('login'))
+		return render_template('signup.html', page_name='signup', username=username, password=pwo)
 
 # @app.route('/chatroom/signup/success')
 # def signup_success():
