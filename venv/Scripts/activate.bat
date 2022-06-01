872049e74d2a299c259a48f91f6670c31c7273cd
Malachi Bergman <malachijb28@gmail.com>
2022-06-01 13:58:45 -0400

    ignore venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\malachi.bergman\Documents\College School\006 Spring 2022\CS 351\Cs 351\venv
+set VIRTUAL_ENV=C:\Users\malachi.bergman\Documents\College School\006 Spring 2022\CS 351\TSpace\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
