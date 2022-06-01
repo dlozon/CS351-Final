61ea6f51491213dff45e79d40b100314517f504a
Malachi Bergman <malachijb28@gmail.com>
2022-06-01 14:43:41 -0400

    send username and password through form post

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -8,15 +8,34 @@ def default():
 	
 @app.route('/chatroom')
 def chatroom():
+	# TODO check cookie user logged in
 	return render_template('chatroom.html', page_name='chatroom')
 	
-@app.route('/chatroom/login')
+@app.route('/chatroom/login', methods=['GET', 'POST'])
 def login():
-	return render_template('login.html', page_name='login')
+	if request.method == "GET":
+		return render_template('login.html', page_name='login')
+	else:
+		username = request.form['username']
+		password = request.form['password']
+		# TODO query for user in database
+		# if user not found: return error message 'User not found' (possibly error page that automatically redirects)
+		# if password doesn't match: error message 'Incorrect Password'
+		# if user found and password matches: render chatroom, logged in (create cookie)
+		return 'Username: %s\nPassword: %s' % (username, password)
 	
-@app.route('/chatroom/signup')
+@app.route('/chatroom/signup', methods=['GET', 'POST'])
 def signup():
-	return render_template('signup.html', page_name='signup')
+	if request.method == "GET":
+		return render_template('signup.html', page_name='signup')
+	else:
+		username = request.form['username']
+		password = request.form['password']
+		# TODO query for username in database
+		# if user exists: error message 'User already exists'
+		# if user not found: create new database user, success message with redirect to chatroom logged in
+		return 'Username: %s\nPassword: %s' % (username, password)
+
 
 # @app.route('/chatroom/signup/success')
 # def signup_success():
